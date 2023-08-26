43cf95a235ec65abc3179f9ac3502e776b76975f
Faby Blanco <60586774+ItsFaby@users.noreply.github.com>
2023-08-25 22:28:53 -0600

    Modified Employee Component

--- Frontend/src/app/Pages/Administracion/empleados/empleados.component.ts
+++ Frontend/src/app/Pages/Administracion/empleados/empleados.component.ts
@@ -5,28 +5,30 @@ import { Table } from 'primeng/table';
 import { ConfirmationService, MessageService } from 'primeng/api';
 
 //Services & Interfaces
-import { Empleados, Puestos, Restaurante } from 'src/Interfaces/Administracion';
-import { EmpleadosService } from 'src/Services/Administracion/empleados.service';
-import { Paises } from 'src/Interfaces/Seguridad';
+import { IEmployee } from 'src/Interfaces/employee';
+import { IRole } from 'src/Interfaces/role';
+import { IRestaurant } from 'src/Interfaces/restaurant';
+import { ICountry } from 'src/Interfaces/country';
+import { EmployeeService } from 'src/Services/Administracion/employee.service';
 
 @Component({
   selector: 'app-empleados',
   templateUrl: './empleados.component.html',
 })
 export class EmpleadosComponent {
-  empleados: Empleados[];
-  empleado: Empleados;
+  empleados: IEmployee[];
+  empleado: IEmployee;
   loading: boolean = true;
   dialog: boolean;
   submitted: boolean;
-  puestos: Puestos[];
-  nacionalidades: Paises[];
-  restaurantes: Restaurante[];
+  puestos: IRole[];
+  nacionalidades: ICountry[];
+  restaurantes: IRestaurant[];
 
   @ViewChild('dt') table: Table;
 
   constructor(
-    private empleadosService: EmpleadosService,
+    private empleadosService: EmployeeService,
     private messageService: MessageService,
     private confirmationService: ConfirmationService
   ) {}
@@ -37,12 +39,12 @@ export class EmpleadosComponent {
     this.dialog = true;
   }
 
-  editProduct(empleado: Empleados) {
+  editProduct(empleado: IEmployee) {
     this.empleado = { ...empleado };
     this.dialog = true;
   }
 
-  deleteProduct(empleado: Empleados) {
+  deleteProduct(empleado: IEmployee) {
     this.confirmationService.confirm({
       message: 'Está seguro de querer eliminar' + empleado.nombre + '?',
       header: 'Confirmación',

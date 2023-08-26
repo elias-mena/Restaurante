34c88304d7a1d4934d14032612ef3d0b5604e70f
Faby Blanco <60586774+ItsFaby@users.noreply.github.com>
2023-08-25 22:39:00 -0600

    Modified Supplier Type

--- Frontend/src/Interfaces/supplier.ts
+++ Frontend/src/Interfaces/supplier.ts
@@ -1,11 +1,11 @@
 export interface ISupplier {
-  code: string;
-  identification: string;
-  first_purchase: Date;
-  name: string;
-  last_name: string;
-  phone_numbers: [string];
-  email: string;
-  address: string;
-  picture: string;
+  code?: string;
+  identification?: string;
+  first_purchase?: Date;
+  name?: string;
+  last_name?: string;
+  phone_numbers?: [string];
+  email?: string;
+  address?: string;
+  picture?: string;
 }
